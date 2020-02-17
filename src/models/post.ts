008d78aecb9d49ee19047e7131a000916c4b5ff7
윤서현(Yoon Seohyun)(seohyun) <seohyun@ncsoft.com>
2020-02-17 13:19:41 +0900

    change DB Schema, (add timestamp)

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,17 +1,17 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IPost extends Document {
-	postId: number;
 	author: string;
 	title: string;
 	content: string;
 }
 
 const PostSchema: Schema = new Schema({
-	postId: { type: String, required: true, unique: true },
 	author: { type: String, required: true },
 	title: { type: String, required: true },
 	content: { type: String }
+}, {
+	timestamps: true,
 });
 
 const PostModel = mongoose.model<IPost>('Post', PostSchema);
