eb39569a8017c53a51d65b8e081336c10ae753b7
윤서현(Yoon Seohyun)(seohyun) <seohyun@ncsoft.com>
2020-03-09 15:09:06 +0900

    change typo

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,12 +1,12 @@
 import { Application } from 'express';
 import bodyParser from 'body-parser';
 import postRouter from '../routes';
-import errorHandleMiddleware from '../errorHandlerMiddleware';
+import errorHandlerMiddleware from '../errorHandlerMiddleware';
 
 export default async function (app: Application) {
   app.use(bodyParser.json());
   app.use('/', postRouter);
 
-  app.use(errorHandleMiddleware);
+  app.use(errorHandlerMiddleware);
   return app;
 };
