a38b41f09e37851e85aa2475861137dc1e1dcdde
윤서현(Yoon Seohyun)(seohyun) <seohyun@ncsoft.com>
2020-03-13 18:43:19 +0900

    add cors

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,10 +1,13 @@
 import { Application } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
+
 import postRouter from '../routes';
 import errorHandlerMiddleware from '../errorHandlerMiddleware';
 
 export default async function (app: Application) {
   app.use(bodyParser.json());
+  app.use(cors());
   app.use('/', postRouter);
 
   app.use(errorHandlerMiddleware);
