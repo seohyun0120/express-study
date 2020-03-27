b876daadb44feb1c79cc72c9f059da44796c1366
윤서현(Yoon Seohyun)(seohyun) <seohyun@ncsoft.com>
2020-03-27 19:13:30 +0900

    comment에 필요한 라우팅을 추가한다.

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import PostController from './post.controller';
+import CommentController from './comment.controller';
 
 const router = express.Router();
 router.get('/', PostController.getPosts);
 router.get('/:id', PostController.getPost);
 router.post('/', PostController.createPost);
 router.patch('/:id', PostController.updatePost);
+router.patch('/:id/comments', CommentController.createComment);
+router.patch('/:id/comments/:commentId', CommentController.updateComment);
 router.delete('/:id', PostController.deletePost);
+router.delete('/:id/comments/:commentId', CommentController.deleteComment);
 
 export default router;
