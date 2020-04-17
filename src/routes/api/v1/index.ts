c3446315e7a546e68463f7e68539da3b28635d9a
윤서현(Yoon Seohyun)(seohyun) <seohyun@ncsoft.com>
2020-04-17 11:14:51 +0900

    multer를 추가한다.

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import multer from 'multer';
 import PostController from './post.controller';
 import CommentController from './comment.controller';
+import mongooseLoader from '../../../loaders/mongoose'
+import MulterGridfsStorage from 'multer-gridfs-storage';
+
+const storage = new MulterGridfsStorage({
+  db: mongooseLoader(),
+  file: (req, file) => {
+    return {
+      filename: 'file_' + Date.now(),
+      bucketName: 'uploadFiles',
+      ...file
+    }
+  }
+});
+
+const upload = multer({ storage }).single('file');
 
 const router = express.Router();
 router.get('/', PostController.getPosts);
 router.get('/:id', PostController.getPost);
-router.post('/', PostController.createPost);
+router.post('/', upload, PostController.createPost);
 router.patch('/:id', PostController.updatePost);
 router.patch('/:id/comments', CommentController.createComment);
 router.patch('/:id/comments/:commentId', CommentController.updateComment);
